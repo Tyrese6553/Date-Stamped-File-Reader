d51fb910864caf3dea74bc026160110e33453ffa
Tyrese6553 <tmathenlall@gmail.com>
2025-10-29 17:45:54 +0200

    Add user input and refactor file matching logic

--- reader.py
+++ reader.py
@@ -3,18 +3,19 @@ import sys
 import re
 
 def main():
-    #file = "Y2000M01D01"
+    y_input = int(input())
+    m_input = int(input())
+    d_input = int(input())
 
     path = r"C:\testing"
     files = os.scandir(path)
     new_files = []
+    pattern = r"^Y([0-9]{4})M(0[0-9]|1[0-2])D(0[1-9]|1[0-9]|2[0-9]|3[0-1])\.[0-9]{4}$"
     for file in files:
-        if file.is_file():
+        if file.is_file() and re.match(pattern, file.name):
             new_files.append(file.name)
 
-
     date_list = []
-    pattern = r"^Y([0-9]{4})M(0[0-9]|1[0-2])D(0[1-9]|1[0-9]|2[0-9]|3[0-1])\.[0-9]{4}$"
     for file in new_files:
         date = []
         matches = re.match(pattern, file)
@@ -26,10 +27,10 @@ def main():
         date.append(m)
         date.append(d)
         date_list.append(date)
-    print(date_list)
 
-
-
+    for i, file in enumerate(date_list):
+        if file[0] == y_input and file[1] == m_input and file[2] == d_input:
+            print(new_files[i])
 
 
 
