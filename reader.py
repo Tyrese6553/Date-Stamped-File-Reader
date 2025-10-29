71a9a18d92200d05a3f7d73d6dd18fdeae1f874e
Tyrese6553 <tmathenlall@gmail.com>
2025-10-29 17:21:33 +0200

    File matching and formatting

--- reader.py
+++ reader.py
@@ -1,20 +1,32 @@
 import os
 import sys
+import re
 
 def main():
-    file = sys.argv[1]
+    #file = "Y2000M01D01"
 
     path = r"C:\testing"
-    files = os.listdir(path)
+    files = os.scandir(path)
+    new_files = []
+    for file in files:
+        if file.is_file():
+            new_files.append(file.name)
 
-    new_files = [os.path.splitext(file)[0] for file in files]
-    
-    print(new_files)
-    
-    if file in new_files:
-        print(file)
-    else:
-        print("File not found")
+
+    date_list = []
+    pattern = r"^Y([0-9]{4})M(0[0-9]|1[0-2])D(0[1-9]|1[0-9]|2[0-9]|3[0-1])\.[0-9]{4}$"
+    for file in new_files:
+        date = []
+        matches = re.match(pattern, file)
+        if matches:
+            y = int(matches.group(1))
+            m = int(matches.group(2))
+            d = int(matches.group(3))
+        date.append(y)
+        date.append(m)
+        date.append(d)
+        date_list.append(date)
+    print(date_list)
 
 
 
