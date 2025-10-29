6c7e12fa46c6c33a54f964adaa848d4d68e99973
Tyrese6553 <tmathenlall@gmail.com>
2025-10-29 19:40:40 +0200

    Implement GUI with file reading and user input for file filtering

--- reader.py
+++ reader.py
@@ -1,38 +1,142 @@
 import os
-import sys
 import re
+import sys
+import tkinter
+from tkinter import messagebox
+from tkinter import filedialog
 
-def main():
-    y_input = int(input())
-    m_input = int(input())
-    d_input = int(input())
+class GUI:
+    def __init__(self):
+        self.path = filedialog.askdirectory()
 
-    path = r"C:\testing"
-    files = os.scandir(path)
-    new_files = []
-    pattern = r"^Y([0-9]{4})M(0[0-9]|1[0-2])D(0[1-9]|1[0-9]|2[0-9]|3[0-1])\.[0-9]{4}$"
-    for file in files:
-        if file.is_file() and re.match(pattern, file.name):
-            new_files.append(file.name)
+        self.files = self.read_files()
 
-    date_list = []
-    for file in new_files:
-        date = []
-        matches = re.match(pattern, file)
-        if matches:
-            y = int(matches.group(1))
-            m = int(matches.group(2))
-            d = int(matches.group(3))
-        date.append(y)
-        date.append(m)
-        date.append(d)
-        date_list.append(date)
+        self.root = tkinter.Tk()
 
-    for i, file in enumerate(date_list):
-        if file[0] == y_input and file[1] == m_input and file[2] == d_input:
-            print(new_files[i])
+        self.root.title("File Reader App")
+        self.root.geometry("470x580")
+        self.root.configure(bg="#4A4A4A")
+
+        tkinter.Label(self.root, 
+                      text=f"Path: {self.path}",
+                      bg="#4A4A4A", 
+                      fg="white", 
+                      font=("Roboto", 14,)
+                      ).grid(row=0, columnspan=2, pady=5)
+
+        tkinter.Label(self.root, text="Year:", 
+                      bg="#4A4A4A", 
+                      fg="white", 
+                      font=("Roboto", 14)
+                      ).grid(row=1, column=0, pady=5)
+        
+        tkinter.Label(self.root, 
+                      text="Month:", 
+                      bg="#4A4A4A", 
+                      fg="white", 
+                      font=("Roboto", 14)
+                      ).grid(row=2, column=0, pady=5)
+        
+        tkinter.Label(self.root, 
+                      text="Day:", 
+                      bg="#4A4A4A", 
+                      fg="white", 
+                      font=("Roboto", 14)
+                      ).grid(row=3, column=0, pady=5)
+
+        self.year_entry = tkinter.Entry(self.root, font=("Roboto", 14))
+        self.year_entry.grid(row=1, column=1)
+        self.month_entry = tkinter.Entry(self.root, font=("Roboto", 14))
+        self.month_entry.grid(row=2, column=1)
+        self.day_entry = tkinter.Entry(self.root, font=("Roboto", 14))
+        self.day_entry.grid(row=3, column=1)
+
+        self.root.bind("<Return>", self.enter_shortcut)
+        self.root.bind("<Control-r>", self.refresh_shortcut)
+        
+        tkinter.Button(self.root, 
+                       text="Read Files", 
+                       command=self.find_file, 
+                       bg="#4A4A4A", 
+                       fg="white", 
+                       font=("Roboto", 14),
+                       activebackground="#8E4585"
+                       ).grid(row=4, columnspan=2, padx=(30, 0), pady=5, sticky="we")
+
+        tkinter.Button(self.root, 
+                text="Refresh", 
+                command=self.refresh,
+                bg="#4A4A4A", 
+                fg="white", 
+                font=("Roboto", 14),
+                activebackground="#8E4585"
+                ).grid(row=5, columnspan=2, padx=(30, 0), pady=5, sticky="we")
+
+        self.scroller = tkinter.Scrollbar(self.root)
+        self.scroller.grid(row=6, column=3, pady=5, sticky="ns")
+
+        self.output_list = tkinter.Listbox(self.root, height=16, width=47, font=("Roboto", 12))
+        self.output_list.grid(row=6, columnspan=2, padx=(12, 0), pady=5)
+
+        self.output_list.config(yscrollcommand= self.scroller.set)
+        self.scroller.config(command=self.output_list.yview)
+
+        self.root.mainloop()
 
 
+    def read_files(self):
+        self.files = []
+        try:
+            pattern = r"^Y([0-9]{4})M(0[0-9]|1[0-2])D(0[1-9]|1[0-9]|2[0-9]|3[0-1])\.[0-9]{4}$"
+            date_list = []
+            for file in os.scandir(self.path):
+                matches = re.match(pattern, file.name)
+                if file.is_file() and matches:
+                    self.files.append(file.name)
+                    date = []
+                    y = int(matches.group(1))
+                    m = int(matches.group(2))
+                    d = int(matches.group(3))
+                    date.append(y)
+                    date.append(m)
+                    date.append(d)
+                    date_list.append(date)
+        except FileNotFoundError:
+            messagebox.showerror("No Directory!", "No directory selected")
+            sys.exit()
+        return date_list
+
+
+    def refresh(self):
+        self.files = self.read_files()
+        self.find_file()
+
+
+    def find_file(self):
+        date_list = self.read_files()
+        try:
+            c = int(self.year_entry.get().strip())
+            r = int(self.month_entry.get().strip())
+            b = int(self.day_entry.get().strip())
+
+            self.output_list.delete(0, tkinter.END)
+            found = False
+            for index, filename in enumerate(date_list):
+                if c == filename[0] and r == filename[1] and b == filename[2]:
+                    self.output_list.insert(tkinter.END, self.files[index])
+                    found = True
+            if not found:
+                self.output_list.insert(tkinter.END, "No matching files")
+        except ValueError:
+            messagebox.showerror("Value Error!", "Enter value(s)")
+
+
+    def enter_shortcut(self, event):
+        self.find_file()
+
+
+    def refresh_shortcut(self, event):
+        self.refresh()
 
 if __name__ == "__main__":
-    main()
+    GUI()
